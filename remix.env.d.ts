8589a9c6d311f0152eca4c9f6d0c7dfc71d46178
Andres Lemus <aclmadrid04@gmail.com>
2022-11-05 09:48:38 -0500

    feat: add api key

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,3 +1,9 @@
 /// <reference types="@remix-run/dev" />
 
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    readonly API_KEY: string;
+  }
+}
+
 /// <reference types="@remix-run/node/globals" />
