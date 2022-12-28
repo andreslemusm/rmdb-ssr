96646f841fdaae42b739b5ecfea7294b832ad4fa
Andres Lemus <aclmadrid04@gmail.com>
2022-12-28 14:58:11 -0500

    feat: remove comment

--- app/root.tsx
+++ app/root.tsx
@@ -23,32 +23,6 @@ const meta: MetaFunction = () => ({
 });
 
 const links: LinksFunction = () => [
-  /*
-   * Favicons
-   * {
-   *   rel: "apple-touch-icon",
-   *   sizes: "180x180",
-   *   href: "/apple-touch-icon.png",
-   * },
-   * {
-   *   rel: "icon",
-   *   type: "image/png",
-   *   sizes: "32x32",
-   *   href: "/favicon-32x32.png",
-   * },
-   * {
-   *   rel: "icon",
-   *   type: "image/png",
-   *   sizes: "16x16",
-   *   href: "/favicon-16x16.png",
-   * },
-   * { rel: "manifest", href: "/site.webmanifest" },
-   * {
-   *   rel: "mask-icon",
-   *   href: "/safari-pinned-tab.svg",
-   *   color: "#101010",
-   * },
-   */
   // Lato 400, 700
   {
     as: "font",
