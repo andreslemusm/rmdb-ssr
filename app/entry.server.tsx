bf69e27d102b612b43e4765329011edbd85e6088
Andres Lemus <aclmadrid04@gmail.com>
2023-09-08 20:45:57 -0500

    feat: add entry files

---
+++ app/entry.server.tsx
@@ -0,0 +1,21 @@
+import type { HandleDocumentRequestFunction } from "@vercel/remix";
+import { RemixServer } from "@remix-run/react";
+import { handleRequest } from "@vercel/remix";
+
+const handleDocumentRequest: HandleDocumentRequestFunction = (
+  request,
+  responseStatusCode,
+  responseHeaders,
+  remixContext
+) => {
+  const remixServer = <RemixServer context={remixContext} url={request.url} />;
+
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixServer
+  ) as Promise<Response>;
+};
+
+export default handleDocumentRequest;
