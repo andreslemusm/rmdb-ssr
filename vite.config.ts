c59be5b8763943ba1689f44d72575e9695aceab5
Andres Lemus <aclmadrid04@gmail.com>
2024-03-02 13:14:34 -0500

    feat: enable vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { installGlobals } from "@remix-run/node";
 import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite";
 
 installGlobals();
 
@@ -17,6 +18,7 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
+      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
