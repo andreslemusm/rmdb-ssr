c060257dd8041bcf801bd49914532265c668b2a8
Andres Lemus <aclmadrid04@gmail.com>
2026-02-25 08:45:04 -0500

    feat: enable rr future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,9 @@ import { vercelPreset } from "@vercel/react-router/vite";
 export default {
   ssr: true,
   presets: [vercelPreset()],
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+  },
 } satisfies Config;
