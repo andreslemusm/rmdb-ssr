9098494c57549ce6ca8bf64bd5a1fb3ccd93411c
Andres Lemus <aclmadrid04@gmail.com>
2026-04-11 14:16:43 -0500

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,14 @@ import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
     reactRouter(),
     tailwindcss(),
-    tsconfigPaths(),
     // `emitFile` is necessary since RR builds more than one bundle!
     visualizer({ emitFile: true }),
   ],
+  resolve: { tsconfigPaths: true },
   server: { port: 3000 },
 })
